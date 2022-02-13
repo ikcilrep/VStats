ef3907f9efeaa18bd7f91c1711eb5b9863e2ff9c
Jakub <jakub@cubepotato.eu>
2022-02-13 17:21:18 +0100

    fix auth

--- src/auth/authCommand.ts
+++ src/auth/authCommand.ts
@@ -5,16 +5,18 @@ import { finalizeLogin } from './authService';
 export const login = (context: vscode.ExtensionContext) => async () => {
     if(context.globalState.get('user')) {
         vscode.window.showInformationMessage('Already logged in.');
+        return;
     }
 
     vscode.env.openExternal(vscode.Uri.parse(API_BASE_URL + '/auth'));
     const res = await vscode.window.showInputBox({
         title: 'Enter auth code',
+        ignoreFocusOut: true,
     });
 
     if(!res) {
         vscode.window.showErrorMessage('Login cancelled.');
-    } 
+    }
 
     await finalizeLogin(context, res!);
 }
