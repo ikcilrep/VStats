850b9077481a2e45386390b01b5a9a4d7bc641eb
Szymon Perlicki <ikcilrep@outlook.com>
2022-02-13 14:56:16 +0100

    Add missing Stats file

---
+++ src/stats.ts
@@ -0,0 +1,30 @@
+import { TextDocument } from "vscode";
+
+interface Statistics {
+    lines: number;
+    chars: number;
+    numberOfFiles: number;
+    createdAt: number;
+}
+
+function statisticsFromDocuments(documents: readonly TextDocument[]): Statistics {
+    const texts = documents.map(document => document.getText());
+    return {
+        "lines": countLines(),
+        "chars": countChars(),
+        "numberOfFiles": documents.length,
+        "createdAt": Date.now()
+    }
+
+    function countLines(): number {
+        return texts.map(text => text.split(/\r\n|\r|\n/).length)
+                    .reduce((result, number_of_lines) => result + number_of_lines);
+    }
+
+    function countChars(): number {
+        return texts.map(text => text.length)
+                    .reduce((result, number_of_chars) => result + number_of_chars);
+    }
+}
+
+export { Statistics, statisticsFromDocuments };
