d79dc1bb9e0a37f1cd6a45d5784282f82563d32d
Jakub <jakub@cubepotato.eu>
2022-02-13 17:13:49 +0100

    ++

--- src/auth/authCommand.ts
+++ src/auth/authCommand.ts
@@ -3,6 +3,10 @@ import { API_BASE_URL } from '../const';
 import { finalizeLogin } from './authService';
 
 export const login = (context: vscode.ExtensionContext) => async () => {
+    if(context.globalState.get('user')) {
+        vscode.window.showInformationMessage('Already logged in.');
+    }
+
     vscode.env.openExternal(vscode.Uri.parse(API_BASE_URL + '/auth'));
     const res = await vscode.window.showInputBox({
         title: 'Enter auth code',
@@ -10,7 +14,7 @@ export const login = (context: vscode.ExtensionContext) => async () => {
 
     if(!res) {
         vscode.window.showErrorMessage('Login cancelled.');
-    }
+    } 
 
     await finalizeLogin(context, res!);
 }
