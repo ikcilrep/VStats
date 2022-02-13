2c7575a5ebcde1436c2da62c81a6663ee621d138
jatoja <fikulatu@gmail.com>
2022-02-13 20:27:28 +0100

    changes

--- src/panels/leaderboardsPanel.ts
+++ src/panels/leaderboardsPanel.ts
@@ -1,5 +1,7 @@
 import * as vscode from "vscode";
 import { getUri } from "../utils/getUri";
+import { fetchStatistics } from "../dataManagement"
+import internal = require("stream");
 
 export class LeaderBoardsPanel {
   public static currentPanel: LeaderBoardsPanel | undefined;
@@ -45,6 +47,8 @@ export class LeaderBoardsPanel {
     }
   }
 
+  
+
   private _getWebviewContent(
     webview: vscode.Webview,
     extensionUri: vscode.Uri
@@ -67,6 +71,60 @@ export class LeaderBoardsPanel {
       })
       return html
     }
+
+    let usernames = "";
+    let lines = "";
+    let characters = "";
+    let files = "";
+    let problems = "";
+    /*let topList = fetchStatistics(context).then(data => {
+      data.array.forEach(val => {
+        html += "<div>"+
+        "<h1> User: "+val.user+"</h1>"+
+        "<h1> Score: "+val.score+"</h1>"+
+        "</div>"
+      });
+    })*/
+
+    /*
+    linesOfCode: number;
+    characterCount: number;
+    fileCount: number;
+    createdAt: number;
+    language: string;
+    projectId: string;
+    */
+
+    let mockList = [
+      {
+        userName:"test1",
+        linesOfCode:2137,
+        characterCount:909,
+        fileCount:69,
+        problems:0,
+      },
+      {
+        userName:"test2",
+        linesOfCode:2137,
+        characterCount:909,
+        fileCount:69,
+        problems:0,
+      },
+      {
+        userName:"test3",
+        linesOfCode:2137,
+        characterCount:909,
+        fileCount:69,
+        problems:0,
+      },
+    ]
+    mockList.forEach(val => {
+      usernames += "<h3> "+val.userName+"</h3>"
+      lines += "<h3> "+val.linesOfCode+"</h3>"
+      characters += "<h3> "+val.characterCount+"</h3>"
+      files += "<h3> "+val.fileCount+"</h3>"
+      problems += "<h3> "+val.problems+"</h3>"
+    })
 
     return /*html*/ `
           <!DOCTYPE html>
@@ -116,23 +174,31 @@ export class LeaderBoardsPanel {
 
                 <div class="leaderboards">
                   <div class="board">
+                    <h2>Username</h2>
+                    <div id="lines-of-code">
+                      ${usernames}
+                    </div>
+                  </div>
+                  <div class="board">
                     <h2>Lines of code</h2>
-                    <div id="lines-of-code"></div>
+                    <div id="lines-of-code">
+                      ${lines}
+                    </div>
                   </div>
                   <hr>
                   <div class="board">
                     <h2>Character count</h2>
-                    <div id="character-count"></div>
+                    <div id="character-count">${characters}</div>
                   </div>
                   <hr>
                   <div class="board">
                     <h2>File count</h2>
-                    <div id="file-count"></div>
+                    <div id="file-count">${files}</div>
                   </div>
                   <hr>
                   <div class="board">
                     <h2>Problem count</h2>
-                    <div id="problem-count">endless<br>more enless</div>
+                    <div id="problem-count">${problems}</div>
                   </div>
                   <hr>
                 </div>
